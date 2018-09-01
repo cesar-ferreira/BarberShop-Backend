51653e6e2ebf74bb77326d9111dfa740d2226e76
cesar-ferreira <cesar.ferreira.tn@gmail.com>
2018-09-01 02:10:15 -0300

    modularizing routes

--- src/app.js
+++ src/app.js
@@ -6,35 +6,17 @@ const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
 
+// Carregar as Rotas
+const indexRoute = require('./routes/index-route');
+const serviceRoute = require('./routes/service-route');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.2"
-    });
-});
 
-const create = router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
 
-const put = router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
-});
+app.use('/', indexRoute);
+app.use('/service', serviceRoute);
 
-const deleted = router.delete('/', (req, res, next) => {
-    res.status(200).send(req.body);
-});
-
-app.use('/', route);
-app.use('/service', create);
-app.use('/service', put);
-app.use('/service', deleted);
 
 module.exports = app;
