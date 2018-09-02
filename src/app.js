21e6c6af012b1eb916ecebdac8aae0b516655046
cesar-ferreira <cesar.ferreira.tn@gmail.com>
2018-09-02 01:42:13 -0300

    creating first service template

--- src/app.js
+++ src/app.js
@@ -2,10 +2,13 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
 
+//Conectando ao banco
+mongoose.connect('mongodb://admin:admin#123@ds018498.mlab.com:18498/db-barbershop');
 // Carregar as Rotas
 const indexRoute = require('./routes/index-route');
 const serviceRoute = require('./routes/service-route');
