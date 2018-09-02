9908b566535b0f950039095a9c8d9cb72f641d19
cesar-ferreira <cesar.ferreira.tn@gmail.com>
2018-09-02 00:37:07 -0300

    creating and modularizing controllers

--- src/routes/service-route.js
+++ src/routes/service-route.js
@@ -3,20 +3,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
+const controller = require('../controllers/service-controller');
 
-router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
-});
-
-router.delete('/', (req, res, next) => {
-    res.status(200).send(req.body);
-});
+router.post('/', controller.post);
+router.put('/:id',controller.put);
+router.delete('/',controller.delete);
 
 module.exports = router;
