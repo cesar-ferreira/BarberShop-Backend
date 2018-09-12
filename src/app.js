fe190c08ea5ee2af8bf28ee887991e87258d1cee
Cesar Ferreira Tavares Neto 112210439 <cesarftn@lcc3-089.lcc.ufcg.edu.br>
2018-09-12 16:53:31 -0300

    implementing user model

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ const serviceRoute = require('./routes/service-route');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-User.create({ name: 'Claudio',username : 'cesar', email: 'claudio@rocketseat.com.br', dateOfBirth: '1993-09-25 21:42:10.946 +00:00', password: '123456' });
+//User.create({ name: 'Claudio',username : 'cesar', email: 'claudio@rocketseat.com.br', dateOfBirth: '1993-09-25 21:42:10.946 +00:00', password: '123456' });
 
 app.post('/register', async (req, res) => {
     const user = await User.create(req.body);
--- src/models/user-model.js
+++ src/models/user-model.js
@@ -2,11 +2,57 @@
 
 module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define('User', {
-        name: DataTypes.STRING,
-        username: DataTypes.STRING,
-        email: DataTypes.STRING,
-        dateOfBirth: DataTypes.DATE,
-        password: DataTypes.STRING,
+        name: {
+            notNull: true,
+            type: DataTypes.STRING,
+            validate: {
+                len: {
+                    args: 3,
+                    msg: "Name must be atleast 3 characters in length"
+                }
+            }
+        },
+        
+        username: {
+            notNull: true,
+            type: DataTypes.STRING,
+            validate: {
+                len: {
+                    args: 3,
+                    msg: "Username must be atleast 3 characters in length"
+                }
+            }
+        },
+
+        email: {
+            notNull: true,
+            type: DataTypes.STRING,
+            validate: {
+                len: {
+                    args: [6, 128],
+                    msg: "Email address must be between 6 and 128 characters in length"
+                },
+                isEmail: {
+                    msg: "Email address must be valid"
+                }
+            }
+        },
+
+        dateOfBirth: {
+            notNull: false,
+            type: DataTypes.DATE,
+        },
+
+        password: {
+            notNull: true,
+            type: DataTypes.STRING,
+            validate: {
+                len: {
+                    args: 3,
+                    msg: "Password must be atleast 3 characters in length"
+                }
+            }
+        }
     });
 
     return User;
