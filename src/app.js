a6b0e558cdc586d12eef8fc8fbf9c4c11368b121
cesar-ferreira <cesar.ferreira.tn@gmail.com>
2018-12-14 00:21:25 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors');
+
 
 const app = express();
+
 
 const setupGraphQL = require('./setup/graphql');
 const setupModules = require('./setup/loadModules');
@@ -10,11 +13,13 @@ const setupModules = require('./setup/loadModules');
 setupGraphQL(app);
 setupModules(app);
 
+app.use(cors())
+
 // Carregar os Models
 
 // Carregar as Rotas
 
-app.use('/swagger-ui.html', express.static(__dirname + '/public/api-docs'));
+//app.use('/swagger-ui.html', express.static(__dirname + '/public/api-docs'));
 
 //User.create({ name: 'Claudio',username : 'cesar', email: 'claudio@rocketseat.com.br', dateOfBirth: '1993-09-25 21:42:10.946 +00:00', password: '123456' });
 
@@ -72,5 +77,5 @@ mutation{
   }
 }
 */
-require('./public/swagger')(app);
+//require('./public/swagger')(app);
 module.exports = app;
